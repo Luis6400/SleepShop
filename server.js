1f029c34eea4d7d790bf17b04a88606b72352484
luisvillarreal <secretangel21@outlook.com>
2023-05-25 01:45:43 -0600

    built a skeleton of all the routes and express code

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
+const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const routes = require('./controllers');
+const exphbs = require('express-handlebars');
+const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -11,15 +14,23 @@ const PORT = process.env.PORT || 3001;
 
 const sess = {
     secret: 'Super secret secret',
-    cookie: {},
+    cookie: {
+      // Stored in milliseconds 
+      maxAge: 6 * 60 *60 * 1000,
+    },
     resave: false,
     saveUninitialized: true,
     store: new SequelizeStore({
       db: sequelize
     })
   };
+
+  const hbs = exphbs.create({ helpers });
   
   app.use(session(sess));
+
+  app.engine('handlebars', exphbs());
+  app.set('view engine', 'handlebars');
   
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
