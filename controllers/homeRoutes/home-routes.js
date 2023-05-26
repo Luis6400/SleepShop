f222223f95d5dcbcceb5d6c8b85db18530d7ff88
luisvillarreal <secretangel21@outlook.com>
2023-05-25 18:44:52 -0600

    updated with qa

---
+++ controllers/homeRoutes/home-routes.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+
+
+router.get('/', async (req, res) => {
+
+
+});
+
+router.get('/login', async (req, res) => {
+
+
+});
+
+router.get('/signup', async (req, res) => {
+
+
+});
+
+router.get('/account', async (req, res) => {
+
+
+});
